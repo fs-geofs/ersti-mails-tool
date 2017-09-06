1c2edaa5034d55ffb4b2a1d91b967472abfb559d
noerw <noerw@gmx.de>
2017-09-06 12:55:42 +0200

    transform erstiwe app to erstimails app

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,9 @@
 'use strict';
 
-// called from menu buttons
-// shows / hides the correct input fields
-function toggleView(mode) {
-  if (mode === 'menu') {
-    $('form').addClass('hidden');
-    $('#back').addClass('hidden');
-    $('#menu').removeClass('hidden');
-    return;
-  }
-
-  if (mode === 'register')
-    $('#form-register').removeClass('hidden');
-  else if (mode === 'waitlist')
-    $('#form-waitlist').removeClass('hidden');
-  else if (mode === 'optout')
-    $('#form-optout').removeClass('hidden');
-
-  $('#menu').addClass('hidden');
-  $('#back').removeClass('hidden');
-}
-
 function showAlert(text, type) {
   // toggle classes for appropiate styling ('success', 'error')
   $('#info').removeClass('success error');
   $('#info').addClass(type);
-
   $('html, body').animate({ scrollTop: 0 }, 400);
   $('#info').html(text).fadeIn(200).delay(5000).fadeOut(400);
 }
--- schema.sql
+++ schema.sql
@@ -5,68 +5,28 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema ersti-we
+-- Schema ersti-mails
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ersti-we` DEFAULT CHARACTER SET utf8 ;
-USE `ersti-we` ;
+CREATE SCHEMA IF NOT EXISTS `ersti-mails` DEFAULT CHARACTER SET utf8 ;
+USE `ersti-mails` ;
 
 -- -----------------------------------------------------
--- Table `ersti-we`.`users`
+-- Table `ersti-mails`.`users`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ersti-we`.`users` ;
+DROP TABLE IF EXISTS `ersti-mails`.`users` ;
 
-CREATE TABLE IF NOT EXISTS `ersti-we`.`users` (
-  `token` CHAR(8) NOT NULL,      -- registration token
-  `firstname` VARCHAR(50) NULL,
-  `lastname` VARCHAR(50) NULL,
-  `gender` ENUM('male', 'female', 'other') NULL,
-  `email` VARCHAR(50) NULL,
-  `phone` VARCHAR(50) NULL,
-  `comment` VARCHAR(500) NULL,
-  `birthday` VARCHAR(50) NULL,
-  `food` ENUM('fleischig', 'vegan', 'vegetarisch') NULL,
-  `study` ENUM('Geoinformatik', 'Geographie', 'Landschaftsökologie', 'Zwei-Fach-Bachelor') NULL,
-  `year` INT NOT NULL, -- assigned year of the user
-  `state` ENUM('free', 'registered', 'opted_out') NOT NULL DEFAULT 'free', -- flag, whether a token is used or not
-  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- date of registration
-  `prev_user` CHAR(8) NULL, -- references the the user previous user of the spot (for waitlist registrations)
-  PRIMARY KEY (`token`, `year`),
-  FOREIGN KEY (`prev_user`) REFERENCES users(`token`)
-) ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `ersti-we`.`waitlist`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `ersti-we`.`waitlist` ;
-
-CREATE TABLE IF NOT EXISTS `ersti-we`.`waitlist` (
-  `email` VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `ersti-mails`.`users` (
+  `email` VARCHAR(50) NOT NULL NULL,
   `year` INT NOT NULL,
-  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`email`, `year`))
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Triggers
--- -----------------------------------------------------
-DROP TRIGGER IF EXISTS `registerUser`;
-
-DELIMITER |
-CREATE TRIGGER `registerUser` BEFORE UPDATE ON `users`
-  FOR EACH ROW BEGIN
-    IF (NEW.state = 'registered') THEN
-      SET NEW.timestamp = CURRENT_TIMESTAMP;
-      DELETE FROM waitlist WHERE email=NEW.email AND year=NEW.year;
-    END IF;
-  END;
-|
-DELIMITER ;
+  `newsletter` BOOLEAN NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- date of registration
+  PRIMARY KEY (`email`, `year`),
+) ENGINE = InnoDB;
 
 -- Compatibility to mysql < 5.6.*, replacing
 -- CREATE USER IF NOT EXISTS `ersti-we`;
-CREATE USER `ersti-we`;
-GRANT ALL PRIVILEGES ON `ersti-we`.* TO `ersti-we`;
+CREATE USER `ersti-mails`;
+GRANT ALL PRIVILEGES ON `ersti-mails`.* TO `ersti-mails`;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
